3355348467e18614830ef3da73b10b8b1c259505
malik <malik.abdulov@gmail.com>
2020-04-28 00:40:08 +0600

    some commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+    <head>
+        <title>TestPage</title>
+        <link href="https://fonts.googleapis.com/css?family=Arimo" rel="stylesheet"/>
+        <link rel="stylesheet" href="/css/fa/all.min.css"/>
+    <style>
+        body {
+            font-family: 'Arimo', sans-serif;
+            background: #fff url(/images/bg.jpg);
+        }
+    </style>
+    </head>
+    <body>
+        <h1 style='text-align:center;'>
+            <a href='/'>
+                <p style='color:#17709c'>Main Page</p>
+            </a>
+        </h1>
+        <p>Welcome to my first website</p>
+        <p>Some text bla</p>
+        <button>First Button</button>
+        <hr>
+        <h3>Links:</h3>
+        <ul>
+            <li>
+                <a href='https://radio.yandex.kz/user/malikabdulov'>My radio</a>
+            </li>
+            <li>
+                <a href='https://music.yandex.kz/'>Yandex Music</a>
+            </li>
+            <li>
+                <a href='https://www.w3schools.com/html/html_basic.asp'>W3School Html</a>
+            </li>
+        </ul>
+        <hr>
+        <h1 style='color:#17709c; font:Arimo;'>Наши цели</h1>
+        <ul>
+            <li>
+                <i class="fas fa-door-closed"></i>
+            </li>
+            <li>
+                <p>Завести технику</p>
+            </li>
+            <li>
+                <p>Обустроить спальное место</p>
+            </li>
+        </ul>
+    </body>
+</html>
